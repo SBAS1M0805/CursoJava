c29dcdee32d0332c9794c5630c2dbee598801566
SBAS1M <sbarrantesa0912@gmail.com>
2026-07-24 14:56:27 -0500

    Cambio

---
+++ Clase1.java
@@ -0,0 +1,21 @@
+public static void main(String[] args) {
+
+    System.out.println("Hola Mundo");
+
+
+}
+
+public class Carro {
+    //Atributos
+    String placa;
+    String marca;
+    int cantidadPasajeros;
+    String color;
+    int modelo;
+
+    //metodos
+    public void acelerar (int velocidadActual, int velocidadFinal) {
+        
+    }
+        
+}
