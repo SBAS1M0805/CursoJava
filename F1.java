c5eaf396015b604b4d6c955a2788e72fe1d6cc63
SBAS1M <sbarrantesa0912@gmail.com>
2026-07-29 15:21:44 -0500

    todo

--- F1.java
+++ F1.java
@@ -4,6 +4,7 @@ public class F1 extends Carro{
 
     //atributos propios de esta clase
     private String Escuderia;
+    private Persona piloto;
 
     //Constructor
     //super que sirve para llamar al constructor de la clase padre
@@ -11,6 +12,12 @@ public class F1 extends Carro{
         super(entradaPlaca, entradaMarca, entradaColor, entradaCantPasajeros, entradaModelo);
         this.Escuderia = entradaEscuderia;
 
+    }
+
+        public F1(String entradaPlaca, String entradaMarca, String entradaColor, int entradaCantPasajeros, int entradaModelo, String entradaEscuderia, Persona entradaObjetoPersona) {
+        super(entradaPlaca, entradaMarca, entradaColor, entradaCantPasajeros, entradaModelo);
+        this.Escuderia = entradaEscuderia;
+        this.piloto = entradaObjetoPersona;    
     }
 
     public void frenar(int velocidadOrigen, int velocidadFinal) {
@@ -24,15 +31,22 @@ public class F1 extends Carro{
         return true;
     }
 
-    public void frenar (int velocidadEntrada) {
-        JOptionPane.showMessageDialog(null, "F1 Frenando");
-    }
+    // public void frenar (int velocidadEntrada) {
+    //    JOptionPane.showMessageDialog(null, "F1 Frenando");
+    
 
     //TIPOS DE POLIMORFISMO
     //POLIMORFISMO DE HERENCIA
     //POLIMORFISMO DE SOBRECARGA
     //POLIMORFISMO ABSTRACTO
+    //Implementacion de una funcion abstracta   
+    //Un constructor se puede sobrecargar 
+    //Asociacion entre clases
+        //Asociaciones Fuertes
+            //conductor - carro es una relacion fuerte
+            //Composicion - se requiere que la clase padre tenga un constructor y la clase hija tambien
+        //Asociaciones Debiles
+            //Agregacion - no se requiere que la clase padre tenga un constructor y la clase hija tambien
 
-
-
+    
 }
