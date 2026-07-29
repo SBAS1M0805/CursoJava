75da8fffbac3edd4b3b4b3e11c76b3cf1b0804a7
SBAS1M <sbarrantesa0912@gmail.com>
2026-07-29 15:04:23 -0500

    act

--- F1.java
+++ F1.java
@@ -1,9 +1,38 @@
+
+
 public class F1 extends Carro{
+
+    //atributos propios de esta clase
+    private String Escuderia;
+
     //Constructor
     //super que sirve para llamar al constructor de la clase padre
-    public F1(String entradaPlaca, String entradaMarca, String entradaColor, int entradaCantPasajeros, int entradaModelo) {
+    public F1(String entradaPlaca, String entradaMarca, String entradaColor, int entradaCantPasajeros, int entradaModelo, String entradaEscuderia) {
         super(entradaPlaca, entradaMarca, entradaColor, entradaCantPasajeros, entradaModelo);
+        this.Escuderia = entradaEscuderia;
+
     }
 
-    
+    public void frenar(int velocidadOrigen, int velocidadFinal) {
+        while (velocidadOrigen > velocidadFinal){
+            System.out.println("Frenando... " + velocidadOrigen);
+            velocidadOrigen-= 40;
+        }
+    }
+
+    public boolean frenar (){
+        return true;
+    }
+
+    public void frenar (int velocidadEntrada) {
+        JOptionPane.showMessageDialog(null, "F1 Frenando");
+    }
+
+    //TIPOS DE POLIMORFISMO
+    //POLIMORFISMO DE HERENCIA
+    //POLIMORFISMO DE SOBRECARGA
+    //POLIMORFISMO ABSTRACTO
+
+
+
 }
--- Principal.java
+++ Principal.java
@@ -1,14 +1,17 @@
+
+
 public class Principal {
 
     public static void main(String[] args) {
 
         Carro objetoCarro = new Carro("JSH876", "Toyota", "Rojo", 5, 2020);
-
+        F1 objetoF1 = new F1("ABC123", "Ferrari", "Rojo", 1, 2022, "Ferrari");
         objetoCarro.setColor("azul");
 
         objetoCarro.acelerar(0, 100);
         System.out.println("El color del carro es: " + objetoCarro.getColor());
 
         objetoCarro.acelerar(20, 80);
+        objetoF1.acelerar(100, 350);
     }
 }
