60fa036c7fab1ac372b43a5458a4609e63d6d746
SBAS1M <sbarrantesa0912@gmail.com>
2026-07-29 13:51:00 -0500

    Actualiza Carro y Principal

--- Carro.java
+++ Carro.java
@@ -1,28 +1,59 @@
 public class Carro {
 
     // Atributos
-    String placa;
-    String marca;
-    int cantidadPasajeros;
-    String color;
-    int modelo;
+    private String placa;
+    private String marca;
+    private int cantidadPasajeros;
+    private String color;
+    private int modelo;
 
-    // Método
+    // Nuevos atributos
+    private boolean lucesEncendidas;
+    private int gasolina = 100;
+
+    // Método para acelerar
     public void acelerar(int velocidadActual, int velocidadFinal) {
         int velocidad = velocidadActual;
 
         while (velocidad < velocidadFinal) {
             velocidad = velocidad + 20;
-            System.out.println("Acelerando! velocidad actual = " + velocidad);
+            System.out.println("Acelerando... velocidad actual = " + velocidad);
         }
     }
 
-    // Método principal
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+    // Método para frenar
+    public void frenar(int velocidadActual) {
+        int velocidad = velocidadActual;
 
-        Carro miCarro = new Carro();
+        while (velocidad > 0) {
+            velocidad = velocidad - 20;
 
-        miCarro.acelerar(0, 100);
+            if (velocidad < 0) {
+                velocidad = 0;
+            }
+
+            System.out.println("Frenando... velocidad actual = " + velocidad);
+        }
+    }
+
+    // Método para encender las luces
+    public void encenderLuces() {
+        lucesEncendidas = true;
+        System.out.println("Las luces están encendidas.");
+    }
+
+    // Método para ver el nivel de gasolina
+    public void verGasolina() {
+        System.out.println("Nivel de gasolina: " + gasolina + "%");
+    }
+
+    // Setter del color
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    // Getter del color
+    public String getColor() {
+        return color;
     }
 }
--- Principal.java
+++ Principal.java
@@ -2,13 +2,28 @@ public class Principal {
 
     public static void main(String[] args) {
 
+        // Crear objeto
         Carro objetoCarro = new Carro();
 
-        objetoCarro.color = "azul";
+        // Asignar color
+        objetoCarro.setColor("Azul");
 
+        // Acelerar
         objetoCarro.acelerar(0, 100);
-        System.out.println("El color del carro es: " + objetoCarro.color);
 
+        // Frenar
+        objetoCarro.frenar(100);
+
+        // Encender luces
+        objetoCarro.encenderLuces();
+
+        // Ver gasolina
+        objetoCarro.verGasolina();
+
+        // Mostrar color
+        System.out.println("El color del carro es: " + objetoCarro.getColor());
+
+        // Volver a acelerar
         objetoCarro.acelerar(20, 80);
     }
 }
